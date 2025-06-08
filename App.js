02a70610f5303c948adc46ae1e61524d1ca0c335
prexpau123@gmail.com <pau@example.com>
2025-06-08 18:26:33 -0500

    Final line endings fix and updates

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/HomeScreen';
+import AddLyricsScreen from './screens/AddLyricsScreen';
+import ViewLyricsScreen from './screens/ViewLyricsScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AddLyrics" component={AddLyricsScreen} />
+        <Stack.Screen name="ViewLyrics" component={ViewLyricsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
