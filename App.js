c6c33f2bafa09a5642b5f918c3d5777202c79776
thangsianpau <prexpau123@gmail.com>
2025-06-13 16:02:11 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import BookFlipScreen from './screens/BookFlipScreen';
 
 <Stack.Screen name="BookFlip" component={BookFlipScreen} />
 
